1456eef3c382abfa2322a373ccb7d45a2d1befdd
Justin Liu <justintwd@gmail.com>
2018-12-16 22:11:32 +0800

    refactor: add  grant-privileges.sh for pma

--- db/mysql/db-1/scripts/enable-pma.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker exec -it mysql-db-1 mysql -uroot \
-      -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-
--- db/mysql/db-3/scripts/enable-pma.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker exec -it mysql-db-3 mysql -uroot \
-      -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-
