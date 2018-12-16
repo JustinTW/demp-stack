d8f4b342d82885be7253d382abc1466303afda4c
Justin Liu <justintwd@gmail.com>
2018-12-16 15:49:23 +0800

    fix: fix env not working

--- db/mysql/db-3/scripts/enable-pma.sh
+++ db/mysql/db-3/scripts/enable-pma.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker exec -it mysql-db-3 mysql -uroot \
-      -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.16.21.%' IDENTIFIED BY 'Moxa89191230' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+      -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
