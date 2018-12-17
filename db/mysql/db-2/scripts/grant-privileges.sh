8017b80896a3119343cde1c73c15a1b0ce3c6cea
Justin Liu <justintwd@gmail.com>
2018-12-17 18:40:52 +0800

    fix: fix grant-privileges not working

--- db/mysql/db-2/scripts/grant-privileges.sh
+++ db/mysql/db-2/scripts/grant-privileges.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-docker exec -it mysql-db-1 chmod 777 /var/lib/mysql -R
+docker exec -it web-stack-mysql-db-1 chmod 777 /var/lib/mysql -R
 
 # root for phpmyadmin
-docker exec -it mysql-db-1 mysql -uroot \
-  -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'pma.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+docker exec -it web-stack-mysql-db-1 mysql -uroot \
+  -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'web-stack-pma.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 # create db for phpmyadmin
-docker exec -it mysql-db-1 mysql -uroot \
+docker exec -it web-stack-mysql-db-1 mysql -uroot \
   -e "CREATE DATABASE phpmyadmin;"  > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
   # pma for phpmyadmin
-  docker exec -it mysql-db-1 mysql -uroot \
-    -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'pma'@'pma.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+  docker exec -it web-stack-mysql-db-1 mysql -uroot \
+    -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'pma'@'web-stack-pma.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
   # import db for phpmyadmin
   docker exec -i mysql-db-1 mysql -uroot \
