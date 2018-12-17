a3fa5ac008542445552eda06bf732de37cc73c30
Justin Liu <justintwd@gmail.com>
2018-12-17 18:38:08 +0800

    feat: add wordpress configs

---
+++ apps/wordpress/.docker/scripts/grant-privileges.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+docker exec -it web-stack-mysql-db-1 chmod 777 /var/lib/mysql -R
+
+# root for phpmyadmin
+docker exec -it web-stack-mysql-db-1 mysql -uroot \
+  -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'pma.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+# create db for phpmyadmin
+docker exec -it web-stack-mysql-db-1 mysql -uroot \
+  -e "CREATE DATABASE phpmyadmin;"  > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  # pma for phpmyadmin
+  docker exec -it web-stack-mysql-db-1 mysql -uroot \
+    -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'pma'@'pma.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+  # import db for phpmyadmin
+  docker exec -i mysql-db-1 mysql -uroot \
+    phpmyadmin < ./data/shared/phpmyadmin.sql
+
+  echo "grant-privileges success !!"
+fi
