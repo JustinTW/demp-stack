cbc7147058667e1579699045ac4fc57ef969d06e
Justin Liu <justintwd@gmail.com>
2018-12-18 11:12:53 +0800

    refactor: move grant privileges to wordpress

--- apps/wordpress/.docker/scripts/grant-privileges.sh
+++ apps/wordpress/.docker/scripts/grant-privileges.sh
@@ -2,22 +2,10 @@
 
 docker exec -it web-stack-mysql-db-1 chmod 777 /var/lib/mysql -R
 
-# root for phpmyadmin
+# create db for wordpress
 docker exec -it web-stack-mysql-db-1 mysql -uroot \
-  -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'pma.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-
-# create db for phpmyadmin
-docker exec -it web-stack-mysql-db-1 mysql -uroot \
-  -e "CREATE DATABASE phpmyadmin;"  > /dev/null 2>&1
-
+  -e "CREATE DATABASE wordpress;"  > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-  # pma for phpmyadmin
   docker exec -it web-stack-mysql-db-1 mysql -uroot \
-    -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'pma'@'pma.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-
-  # import db for phpmyadmin
-  docker exec -i mysql-db-1 mysql -uroot \
-    phpmyadmin < ./data/shared/phpmyadmin.sql
-
-  echo "grant-privileges success !!"
+    -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'web-stack-wordpress.web-stack' IDENTIFIED BY '123456' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 fi
